b17a24c781a21daeaed5861127307a3cc2f90451
guisiq <lopesgui01@hotmail.com>
2021-10-11 10:59:20 -0300

    definindo telas

---
+++ src/main/java/controller/meusLivrosController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class meusLivrosController implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
