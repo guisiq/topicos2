50d6ac5db31225ae6dd1d3089781d6d3df28416d
guisiq <lopesgui01@hotmail.com>
2021-11-30 11:33:10 -0300

    salvando as auteracoes

--- src/main/java/application/Util.java
+++ src/main/java/application/Util.java
@@ -1,12 +1,20 @@
 package application;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.FacesContext;
+import javax.imageio.ImageIO;
 
 import org.apache.commons.codec.digest.DigestUtils;
+
+import servelet.ImgCapaLivroServlet;
+
+
 
 public class Util {
 	
@@ -47,6 +55,36 @@ public class Util {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static boolean saveImageUsuario(InputStream inputStream, String imageType, int idUsuario) {
+		// Exemplo da maquina do janio: /home/janio/images/usuario
+		String diretorio = System.getProperty("user.home") + ImgCapaLivroServlet.PATH_IMAGES_CAPA_LIVRO;
+		
+		// Criando os diretorios caso nao exista
+		File file = new File(diretorio);
+		if (!file.exists()) {
+			file.mkdirs(); // mkdirs - cria arquivo ou diretorio (caso o diretorio anterior nao exista ele cria tambem)
+		}
+		
+		try {
+			// criando o espaco de memoria para armazenamento de uma imagem
+			// inputStream - eh o fluxo de dados de entrada 
+			BufferedImage bImage = ImageIO.read(inputStream);
+			
+			// estrutura final do arquivo ex: /home/janio/images/usuario/01.png
+			File arquivoFinal = new File(diretorio + File.separator + idUsuario + "." + imageType);
+			// salvando a imagem
+			// buffer da imagem, png/gif, 01.png
+			ImageIO.write(bImage, imageType, arquivoFinal);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
 	}
 
 }
